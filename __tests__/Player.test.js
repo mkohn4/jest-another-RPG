a7a4abe61c2112da65babf02dbbcb8ce2638b882
maxkohn <kohn.max@gmail.com>
2021-11-29 16:00:08 -0500

    updated with more player tests and functions

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,4 +1,5 @@
-
+//const { expect } = require('@jest/globals');
+//const { test } = require('jest-circus');
 const Player = require('../lib/Player.js');
 const Potion = require('../lib/Potion')
 jest.mock('../lib/Potion');
@@ -33,4 +34,31 @@ test('gets inventory from player or returns false', () => {
     player.inventory = [];
 
     expect(player.getInventory()).toEqual(false);
-})
+});
+
+test("gets players health value", () => {
+    const player = new Player('Dave');
+
+    expect(player.getHealth()).toEqual(expect.stringContaining(player.health.toString()));
+});
+
+test('checks if player is alive or not', () => {
+    const player = new Player('Dave');
+
+    expect(player.isAlive()).toBeTruthy();
+
+    player.health = 0;
+
+    expect(player.isAlive()).toBeFalsy();
+});
+
+test('subtracts from players health', () =>{
+    const player = new Player('Dave');
+    const oldHealth = player.health;
+
+    player.reduceHealth(5);
+    expect(player.health).toBe(oldHealth - 5);
+
+    player.reduceHealth(99999);
+    expect(player.health).toBe(0);
+});
